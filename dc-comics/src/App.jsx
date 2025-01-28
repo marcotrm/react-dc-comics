ec0bd1374575ddcbc74c4d128e5ae295a2911ec1
marco torremacco <mtorremacco@gmail.comm>
2025-01-28 20:41:35 +0100

    init project

---
+++ dc-comics/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./Components/Header";
+import Main from "./Components/Main";
+import Footer from "./Components/Footer";
+
+function app() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+export default App;
