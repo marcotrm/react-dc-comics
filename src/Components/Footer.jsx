ce3c64e2d57c57ffc80a7355644e48fa1ac922b4
marco torremacco <mtorremacco@gmail.comm>
2025-01-30 15:04:53 +0100

    feat add middle footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,4 +1,5 @@
 import navBarFooterTop from "./data/navBarFooterTop";
+import footerLinks from "./data/navMiddleFooter";
 
 export default function Footer() {
   return (
@@ -16,6 +17,32 @@ export default function Footer() {
                 );
               })}
             </ul>
+          </div>
+        </div>
+        <div className="footer-middle">
+          <div className="container">
+            <div className="footer-nav-middle">
+              {footerLinks.map((section, index) => {
+                return (
+                  <div key={index} className="footer-section">
+                    <h4>{section.category}</h4>
+                    <ul>
+                      {section.links.map((link, i) => {
+                        return (
+                          <li key={i}>
+                            <a href={link.url}>{link.text}</a>
+                          </li>
+                        );
+                      })}
+                    </ul>
+                  </div>
+                );
+              })}
+              <img src="src/assets/img/dc-logo-bg.png" alt="logo-bg" />
+            </div>
+          </div>
+          <div className="footer-bottom">
+            <div className="container"></div>
           </div>
         </div>
       </div>
---
+++ src/Components/data/navMiddleFooter.js
@@ -0,0 +1,52 @@
+const footerLinks = [
+  {
+    category: "DC COMICS",
+    links: [
+      { text: "Characters", url: "#" },
+      { text: "Comics", url: "#" },
+      { text: "Movies", url: "#" },
+      { text: "TV", url: "#" },
+      { text: "Games", url: "#" },
+      { text: "Collectibles", url: "#" },
+      { text: "Videos", url: "#" },
+      { text: "Fans", url: "#" },
+      { text: "News", url: "#" },
+      { text: "Shop", url: "#" },
+    ],
+  },
+  {
+    category: "SHOP",
+    links: [
+      { text: "Shop Dc", url: "#" },
+      { text: "Shop DC Colletbles", url: "#" },
+    ],
+  },
+  {
+    category: "DC",
+    links: [
+      { text: "Terms Of Use", url: "#" },
+      { text: "Privacy Policy (New)", url: "#" },
+      { text: "Ad Choices", url: "#" },
+      { text: "Advertising", url: "#" },
+      { text: "Jobs", url: "#" },
+      { text: "Subscriptions", url: "#" },
+      { text: "Talent Workshop", url: "#" },
+      { text: "CPSC Certificates", url: "#" },
+      { text: "Ratings", url: "#" },
+      { text: "Shop Help", url: "#" },
+      { text: "Contact Us", url: "#" },
+    ],
+  },
+  {
+    category: "SITES",
+    links: [
+      { text: "DC", url: "#" },
+      { text: "MAD Magazine", url: "#" },
+      { text: "DC Kids", url: "#" },
+      { text: "DC Universe", url: "#" },
+      { text: "DC Power Visa", url: "#" },
+    ],
+  },
+];
+
+export default footerLinks;
